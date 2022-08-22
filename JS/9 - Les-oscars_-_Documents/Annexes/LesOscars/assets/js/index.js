993e119599b0616f56746aabe66338e491bbd989
AlbanDev <alban.developpeur@gmail.com>
2022-08-22 13:11:45 +0200

    js - 9

--- JS/9 - Les-oscars_-_Documents/Annexes/LesOscars/assets/js/index.js
+++ JS/9 - Les-oscars_-_Documents/Annexes/LesOscars/assets/js/index.js
@@ -3,21 +3,21 @@ const poneys = [
     name: "Pinky Pie",
     strength: "50",
     speed: "65",
-    charisma: "80",
+    charisma: "80"
   },
   {
     name: "Rainbow Dash",
     strength: "80",
     speed: "75",
-    charisma: "90",
+    charisma: "90"
   },
   {
     name: "Twilight Sparkle",
     strength: "90",
     speed: "45",
-    charisma: "70",
+    charisma: "70"
   },
-  { 
+  {
     name: "Rarity",
     strength: "30",
     speed: "50",
@@ -30,4 +30,3 @@ const poneys = [
     charisma: "30"
   }
 ];
-
